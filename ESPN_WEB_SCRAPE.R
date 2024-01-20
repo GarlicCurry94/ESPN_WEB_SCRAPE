e2504ad27c7755d8d31f9a7f020888fbfc6a58ee
GarlicCurry94 <126899163+GarlicCurry94@users.noreply.github.com>
2024-01-20 14:11:30 +0000

    Add files via upload

---
+++ ESPN_WEB_SCRAPE.R
@@ -0,0 +1,20 @@
+install.packages("rvest")
+install.packages("dplyr")
+install.packages("stringr")
+install.packages("tidyverse")
+install.packages("writex1")
+
+library(rvest)
+library(dplyr)
+library(stringr)
+library(tidyverse)
+library(writexl)
+
+shedule_url <- read_html("https://www.espn.co.uk/nba/team/schedule/_/name/phx/phoenix-suns")
+
+suns_shedule <- shedule_url %>% html_node("table") %>% html_table(fill = TRUE)
+
+data(suns_shedule)
+View(suns_shedule)
+
+write_xlsx(suns_shedule, "Phoenix_Suns_Shedule.xlsx")
